29e603af68c31d46d7a1f42e4c33d1db4ef32a07
Geirmundur Orri Sigurdsson <geirmundur.sigurdsson@nov.cm>
2019-07-28 13:20:58 +0200

    Fixing and adding CMake

--- src/gosmax31865.cpp
+++ src/gosmax31865.cpp
@@ -1,28 +1,36 @@
-#include "gosmax31865.h"
+#include <Arduino.h>
 
+#ifndef GOS_ARDUINO_UNIT_TESTING
 // include Playing With Fusion MAX31865 libraries
 #include <PlayingWithFusion_MAX31865_STRUCT.h>       // struct library
+#endif
+
+#include <arduinosensor.h>
+
+#include "gosmax31865.h"
 
 #define MAX_31865_ERROR_COUNT  7
-#define MAX_31865_ERROR_LENGTH 8
+#define MAX_31865_ERROR_LENGTH 9 /* 8 character plus '\0' */
 
 namespace gos {
 namespace max31865 {
 
+#ifndef GOS_ARDUINO_UNIT_TESTING
 // structures for holding RTD values
 static PWFusion_MAX31865_RTD* pwf_max_rtd;
 struct var_max31865 max_rtd_values;
+#endif
 
 static const char max31865errortext[MAX_31865_ERROR_COUNT][MAX_31865_ERROR_LENGTH] = {
   "RTD HiTh",
-  "RTD LoTh",
+  "RTD LoTh" ,
   "REF High",
   "FORCE",
   "FORCE",
   "Voltage",
   "Unknown"
 };
-static const unit8_t max31865errormasks[MAX_31865_ERROR_COUNT =
+static const uint8_t max31865errormasks[MAX_31865_ERROR_COUNT] =
 {
     0x80,
     0x40,
@@ -30,18 +38,19 @@ static const unit8_t max31865errormasks[MAX_31865_ERROR_COUNT =
     0x10,
     0x08,
     0x04,
-    0x00
+    0xff
 };
 
-static const char* error;
+static const char* Error;
 static uint8_t i;
 
+#ifndef GOS_ARDUINO_UNIT_TESTING
 bool setup(
   const uint8_t& pincs,
   const uint8_t& rtdtype,
   const uint8_t& wires,
   const int& spispeed,
-  const uint8_t& spidatamode) {  
+  const uint8_t& spidatamode) {
   // setup for the the SPI library:
   SPI.begin();                            // begin SPI
   SPI.setClockDivider(spispeed);   // SPI speed to SPI_CLOCK_DIV16 (1MHz)
@@ -60,7 +69,7 @@ bool setup(
 double read(uint8_t& status, uint8_t& fault) {
   /* Read max */
   pwf_max_rtd->MAX31865_full_read(&max_rtd_values);
-  if(max_rtd_values.status == 0) {
+  if (max_rtd_values.status == 0) {
     gos::sensor::value = (((double)max_rtd_values.rtd_res_raw) / 32.0) - 256.0;
     status = gos::sensor::range::check(reading);
     return gos::sensor::value;
@@ -69,20 +78,19 @@ double read(uint8_t& status, uint8_t& fault) {
     fault = max_rtd_values.status;
   }
 }
+#endif
 
 const char* error(const uint8_t& status, const uint8_t& fault) {
-  if((error = gos::sendor::error(status)) != nullptr) {
-    return error;
+  if ((Error = ::gos::sensor::error(status)) != nullptr) {
+    return Error;
   } else {
-    for(i = 0; i < MAX_31865_ERROR_COUNT; i++) {
-      if(max31865errormasks[i] & fault) {
+    for (i = 0; i < MAX_31865_ERROR_COUNT; i++) {
+      if (max31865errormasks[i] & fault) {
         return max31865errortext[i];
       }
     }
   }
 }
 
-}
-}
 }
 }
--- src/gosmax31865.h
+++ src/gosmax31865.h
@@ -1,15 +1,19 @@
 #ifndef _GOS_ARDUINO_LIBRARY_MAX_31865_H_
 #define _GOS_ARDUINO_LIBRARY_MAX_31865_H_
 
-#include <SPI.h>
+#include <Arduino.h>
 
+#ifndef GOS_ARDUINO_UNIT_TESTING
+#include <SPI.h>
 #include <PlayingWithFusion_MAX31865.h>              // core library
+#endif
 
 #define RTD_TYPE_PT100         1 
 #define RTD_TYPE_PT1000        2
 
 namespace gos {
 namespace max31865 {
+#ifndef GOS_ARDUINO_UNIT_TESTING
 void setup(
   const uint8_t& pincs = 10,                  /* PIN for Chip Select CS is often 10 */
   const uint8_t& rtdtype = RTD_TYPE_PT100,    /* RTD type either PT100 or PT1000 */
@@ -17,6 +21,7 @@ void setup(
   const int& spispeed = SPI_CLOCK_DIV16,      /* SPI_CLOCK_DIV16 (1MHz) */
   const uint8_t& spidatamode = SPI_MODE3);    /* MAX31865 works in MODE1 or MODE3 */
 double read(uint8_t& status, uint8_t& fault);
+#endif
 const char* error(const uint8_t& status, const uint8_t& fault);
 }
 }
