c2dd8ae214bc613bf4e2c3ab844b98582adc07bf
Geirmundur Orri Sigurdsson <geirmundur.sigurdsson@nov.cm>
2019-07-31 15:52:45 +0200

    Changed to class

--- src/gosmax31865.cpp
+++ src/gosmax31865.cpp
@@ -1,35 +1,23 @@
 #include <Arduino.h>
 
-#ifndef GOS_ARDUINO_UNIT_TESTING
-// include Playing With Fusion MAX31865 libraries
-#include <PlayingWithFusion_MAX31865_STRUCT.h>       // struct library
-#endif
-
-#define GOS_SENSOR_GLOBAL
-#include <arduinosensor.h>
-
 #include "gosmax31865.h"
 
+#define MAX_31865_OK           0
 #define MAX_31865_ERROR_COUNT  7
-#define MAX_31865_ERROR_LENGTH 9 /* 8 character plus '\0' */
+#define MAX_31865_ERROR_LENGTH 8 /* 7 character plus '\0' */
+
+#define GOS_MAX_31865_ERRT_RTD_HITH  "RTD HTh"
+#define GOS_MAX_31865_ERRT_RTD_LOTH  "RTD LTh"
+#define GOS_MAX_31865_ERRT_RTD_REF   "REF Hi"
+#define GOS_MAX_31865_ERRT_RTD_FORCE "FORCE"
+#define GOS_MAX_31865_ERRT_RTD_VOLT  "Volt"
+#define GOS_MAX_31865_ERRT_RTD_UNKN  "Unkn"
 
 namespace gos {
+
 namespace max31865 {
-
-#ifndef GOS_ARDUINO_UNIT_TESTING
-// structures for holding RTD values
-static PWFusion_MAX31865_RTD* pwf_max_rtd;
-struct var_max31865 max_rtd_values;
-#endif
-
-#define GOS_MAX_31865_ERRT_RTD_HITH  "RTD HiTh"
-#define GOS_MAX_31865_ERRT_RTD_LOTH  "RTD LoTh"
-#define GOS_MAX_31865_ERRT_RTD_REF   "REF High"
-#define GOS_MAX_31865_ERRT_RTD_FORCE "FORCE"
-#define GOS_MAX_31865_ERRT_RTD_VOLT  "Voltage"
-#define GOS_MAX_31865_ERRT_RTD_UNKN  "Unknown"
-
-static const char max31865errortext[MAX_31865_ERROR_COUNT][MAX_31865_ERROR_LENGTH] = {
+namespace error {
+static const char text[MAX_31865_ERROR_COUNT][MAX_31865_ERROR_LENGTH] = {
   GOS_MAX_31865_ERRT_RTD_HITH,
   GOS_MAX_31865_ERRT_RTD_LOTH,
   GOS_MAX_31865_ERRT_RTD_REF,
@@ -38,8 +26,7 @@ static const char max31865errortext[MAX_31865_ERROR_COUNT][MAX_31865_ERROR_LENGT
   GOS_MAX_31865_ERRT_RTD_VOLT,
   GOS_MAX_31865_ERRT_RTD_UNKN
 };
-
-static const uint8_t max31865errorlength[MAX_31865_ERROR_COUNT] = {
+static const uint8_t length[MAX_31865_ERROR_COUNT] = {
   sizeof(GOS_MAX_31865_ERRT_RTD_HITH),
   sizeof(GOS_MAX_31865_ERRT_RTD_LOTH),
   sizeof(GOS_MAX_31865_ERRT_RTD_REF),
@@ -48,8 +35,7 @@ static const uint8_t max31865errorlength[MAX_31865_ERROR_COUNT] = {
   sizeof(GOS_MAX_31865_ERRT_RTD_VOLT),
   sizeof(GOS_MAX_31865_ERRT_RTD_UNKN)
 };
-
-static const uint8_t max31865errormasks[MAX_31865_ERROR_COUNT] =
+static const uint8_t mask[MAX_31865_ERROR_COUNT] =
 {
     0x80,
     0x40,
@@ -59,58 +45,145 @@ static const uint8_t max31865errormasks[MAX_31865_ERROR_COUNT] =
     0x04,
     0xff
 };
-
-static const char* Error;
-static uint8_t i;
-
-#ifndef GOS_ARDUINO_UNIT_TESTING
-void setup(
-  const uint8_t& pincs,                        /* PIN for Chip Select CS is often 10 */
-  const uint8_t& rtdtype,                     /* RTD type either PT100 or PT1000 */
-  const uint8_t& wires,                       /* Often 3 wires rather than 4 */
-  const int& spispeed,                        /* SPI_CLOCK_DIV16 (1MHz) */
-  const uint8_t& spidatamode) {               /* MAX31865 works in MODE1 or MODE3 */
-  // setup for the the SPI library:
-  SPI.begin();                            // begin SPI
-  SPI.setClockDivider(spispeed);   // SPI speed to SPI_CLOCK_DIV16 (1MHz)
-  SPI.setDataMode(spidatamode);             // MAX31865 works in MODE1 or MODE3
-
-    // initalize the chip select pin
-  pinMode(pincs, OUTPUT);
-
-  pwf_max_rtd = new PWFusion_MAX31865_RTD(pincs);
-
-  // configure channel 0
-  pwf_max_rtd->MAX31865_config(&max_rtd_values, wires);
-  max_rtd_values.RTD_type = rtdtype;
+}
 }
 
-double read(uint8_t& status, uint8_t& fault) {
-  /* Read max */
-  pwf_max_rtd->MAX31865_full_read(&max_rtd_values);
-  if (max_rtd_values.status == 0) {
-    gos::sensor::value = (((double)max_rtd_values.rtd_res_raw) / 32.0) - 256.0;
-    status = gos::sensor::range::check(reading);
-    return gos::sensor::value;
-  } else {
-    status = GOS_SENSOR_STATUS_ERROR;
-    fault = max_rtd_values.status;
-  }
+Max31865::Max31865(
+  const uint8_t&  pincs,    /* PIN for Chip Select CS is often 10 */
+  const uint32_t& clock,    /* SPI CLOCK 1000000 (1MHz) */
+  const uint8_t&  order,    /* SPI bit order MSB FIRST */
+  const uint8_t&  mode) :   /* MAX6677 works in MODE1 (or MODE3?) */
+  pincs_(pincs), spisettings_(clock, order, mode) {
 }
+
+void Max31865::initialize(
+  const uint8_t& rtdtype,   /* RTD type either PT100 or PT1000 */
+  const uint8_t& wires) {   /* Often 3 wires rather than 4 */
+
+  wires_ = wires;
+
+  pinMode(pincs_, OUTPUT);
+
+#ifdef DELAY_INITIALIZE_MAX_31865
+  // immediately pull CS pin high to avoid conflicts on SPI bus
+  digitalWrite(pincs_, HIGH);
+  delay(DELAY_INITIALIZE_MAX_31865);
 #endif
 
-const char* error(const uint8_t& status, const uint8_t& fault, uint8_t& length) {
-  if ((Error = ::gos::sensor::error(status, length)) != nullptr) {
-    return Error;
+  SPI.beginTransaction(spisettings_);
+
+  // take the chip select low to select the device:
+  digitalWrite(pincs_, LOW);
+
+  configure();
+
+  // take the chip select high to de-select, finish config write
+  digitalWrite(pincs_, HIGH);
+
+  SPI.endTransaction();
+
+  rtdtype_ = rtdtype;
+}
+
+bool Max31865::read(double& value) {
+  SPI.beginTransaction(spisettings_);
+
+  // must set CS low to start operation
+  digitalWrite(pincs_, LOW);
+
+  // Write command telling IC that we want to 'read' and start at register 0
+  SPI.transfer(0x00);				// plan to start read at the config register
+
+  //
+  // read registers in order
+  //
+
+  // configuration
+  conf_reg_ = SPI.transfer(0x00);	        // read 1st 8 bits
+
+#ifndef SPI_NOT_USE_16_MAX_31865
+  rtd_res_raw_ = SPI.transfer16(0x0000);  // RTD MSBs and LSBs
+#else
+  rtd_res_raw_  = SPI.transfer(0x00);     // RTD MSBs
+  rtd_res_raw_ <<= 8;
+  rtd_res_raw_ |= SPI.transfer(0x00);     // RTD LSBs
+#endif
+  rtd_res_ = rtd_res_raw_ >> 1;           // store data after 1-bit right shift
+
+#ifndef SPI_NOT_USE_16_MAX_31865
+  hft_val_ = SPI.transfer16(0x0000);      // High Fault Threshold MSB and LSB
+#else
+  hft_val_ = SPI.transfer(0x00);          // High Fault Threshold MSB
+  hft_val_ <<= 8;
+  hft_val_ |= SPI.transfer(0x00);         // High Fault Threshold LSB
+#endif
+  hft_val_ >>= 1;                         // store data after 1-bit right shift
+
+#ifndef SPI_NOT_USE_16_MAX_31865
+  lft_val_ = SPI.transfer16(0x0000);      // Low Fault Threshold MSB and LSB  
+#else
+  lft_val_  = SPI.transfer(0x00);         // Low Fault Threshold MSB
+  lft_val_ <<= 8;
+  lft_val_ |= SPI.transfer(0x00);         // Low Fault Threshold MSB
+#endif
+  lft_val_ >>= 1;                         // store data after 1-bit right shift
+
+  status_ = SPI.transfer(0x00);		        // Fault Status read 8th 8 bits
+
+  if (rtd_res_raw_ && status_ == MAX_31865_OK) {
+    // Calculate deg C
+    value = (((double)rtd_res_) / 32.0) - 256.0;
   } else {
-    for (i = 0; i < MAX_31865_ERROR_COUNT; i++) {
-      if (max31865errormasks[i] & fault) {
-        length = max31865errorlength[i];
-        return max31865errortext[i];
+    configure();
+  }
+
+  // set CS high to finish read
+  digitalWrite(pincs_, HIGH);
+
+  SPI.endTransaction();
+
+  return rtd_res_raw_ && status_ == MAX_31865_OK;
+}
+
+#ifndef SPI_NO_ERROR_HANDLING_MAX_31865
+const char* Max31865::error(uint8_t& length) {
+  if (status_ != MAX_31865_OK) {
+    for (uint8_t i = 0; i < MAX_31865_ERROR_COUNT; i++) {
+      if (::gos::max31865::error::mask[i] & status_) {
+        length = ::gos::max31865::error::length[i];
+        return ::gos::max31865::error::text[i];
       }
     }
   }
+  length = 0;
+  return nullptr;
+}
+#else
+const uint8_t& Max31865::status() {
+  return status_;
+}
+#endif
+
+void Max31865::configure() {
+  // Write config to MAX31865 chip
+  SPI.transfer(0x80);           // Send config register location to chip
+                                // 0x8x to specify 'write register value' 
+                                // 0xx0 to specify 'configuration register'
+  SPI.transfer(0xC2 | wires_);  // Write config to IC
+                                // bit 7: Vbias -> 1 (ON)
+                                // bit 6: conversion mode -> 1 (AUTO)
+                                // bit 5: 1-shot -> 0 (off)
+                                // bit 4: 3-wire select -> 0 (2/4 wire config)
+                                // bit 3-2: fault detection cycle -> 0 (none)
+                                // bit 1: fault status clear -> 1 (clear any fault)
+                                // bit 0: 50/60 Hz filter select -> 0 (60 Hz)
+
+    // code if you need to configure High and Low fault threshold registers (4 total registers)
+//  SPI.transfer(0x83);  // write cmd, start at HFT MSB reg (0x83)
+//  SPI.transfer(0xFF);  // write cmd, start at HFT MSB reg (0x83)
+//  SPI.transfer(0xFF);  // write cmd, start at HFT MSB reg (0x83)
+//  SPI.transfer(0x00);  // write cmd, start at HFT MSB reg (0x83)
+//  SPI.transfer(0x00);  // write cmd, start at HFT MSB reg (0x83)
 }
 
-}
 }
--- src/gosmax31865.h
+++ src/gosmax31865.h
@@ -3,27 +3,55 @@
 
 #include <Arduino.h>
 
-#ifndef GOS_ARDUINO_UNIT_TESTING
 #include <SPI.h>
-#include <PlayingWithFusion_MAX31865.h>              // core library
-#endif
 
-#define RTD_TYPE_PT100         1 
-#define RTD_TYPE_PT1000        2
+#define SPI_CLOCK_MAX_31865        1000000 /* (1MHz) */
+#define SPI_MODE_MAX_31865       SPI_MODE3
+#define SPI_BIT_ORDER_MAX_31865   MSBFIRST
+
+#define RTD_TYPE_PT100                   1
+#define RTD_TYPE_PT1000                  2
+
+#define RTD_2_WIRE		                0x00
+#define	RTD_3_WIRE		                0x10
+#define	RTD_4_WIRE		                0x00
+
+//#define DELAY_INITIALIZE_MAX_31865     100
 
 namespace gos {
-namespace max31865 {
-#ifndef GOS_ARDUINO_UNIT_TESTING
-void setup(
-  const uint8_t& pincs = 10,                  /* PIN for Chip Select CS is often 10 */
-  const uint8_t& rtdtype = RTD_TYPE_PT100,    /* RTD type either PT100 or PT1000 */
-  const uint8_t& wires = RTD_3_WIRE,          /* Often 3 wires rather than 4 */
-  const int& spispeed = SPI_CLOCK_DIV16,      /* SPI_CLOCK_DIV16 (1MHz) */
-  const uint8_t& spidatamode = SPI_MODE3);    /* MAX31865 works in MODE1 or MODE3 */
-double read(uint8_t& status, uint8_t& fault);
+class Max31865 {
+public:
+  Max31865(
+    const uint8_t&  pincs = 10,                       /* PIN for Chip Select CS is often 10 */
+    const uint32_t& clock = SPI_CLOCK_MAX_31865,      /* SPI CLOCK 1000000 (1MHz) */
+    const uint8_t&  order = SPI_BIT_ORDER_MAX_31865,  /* SPI bit order MSB FIRST */
+    const uint8_t&  mode = SPI_MODE_MAX_31865);       /* MAX31865 works in MODE1 or MODE3 */
+  void initialize(
+    const uint8_t& rtdtype = RTD_TYPE_PT100,          /* RTD type either PT100 or PT1000 */
+    const uint8_t& wires = RTD_3_WIRE);               /* Often 3 wires rather than 4 */
+  bool read(double& value);
+#ifndef SPI_NO_ERROR_HANDLING_MAX_31865
+  const char* error(uint8_t& length);
+#else
+  const uint8_t& status();
 #endif
-const char* error(const uint8_t& status, const uint8_t& fault, uint8_t& length);
-}
+private:
+  uint8_t pincs_;
+  SPISettings spisettings_;
+
+  uint8_t rtdtype_;       // RTD type. 1 = PT100; 2 = PT1000
+  uint8_t wires_;         // # of wires. 2/4-wire: 0x00, 3-wire: 0x10
+
+  uint8_t  conf_reg_;			// Configuration register readout
+  uint16_t rtd_res_raw_;  // RTD IC raw resistance register
+  uint16_t rtd_res_;      // RTD IC resistance
+  int16_t  hft_val_;			// High fault threshold register readout
+  int16_t  lft_val_;			// Low fault threshold register readout
+  uint8_t  status_;			  // RTD status - full status code
+
+  void configure();
+};
+
 }
 
 #endif /* _GOS_ARDUINO_LIBRARY_MAX_31865_H_ */
